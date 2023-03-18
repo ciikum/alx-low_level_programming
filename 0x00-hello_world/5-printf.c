0331581fbe4a9e6957eb9f7539b5bf779c6a9c65
ciikum <wanjikumsarabi@gmail.com>
2023-03-18 06:41:46 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdioh.>
+/**
+ * main - program that prints a line using printf
+ * Return : 0 (all done)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n ")
+	return (0);
+}
