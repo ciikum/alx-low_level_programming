86d256b09ef9fa946480e5d091fc52bc787a3726
ciikum <wanjikumsarabi@gmail.com>
2023-03-18 04:28:02 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line w puts function folowed by a new line
+ * Return: 0 (all good)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle.");
+return (0);
+}
