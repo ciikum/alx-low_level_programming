870e22810fecd65f647f6c26e6df0557e1ee2c01
ciikum <wanjikumsarabi@gmail.com>
2023-03-28 12:27:14 -0700

    half of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * puts2 - function should print only one character out of two
  * starting with the first one
  * @str: input
  * Return: print
  */
-
 void puts2(char *str)
 {
 	int longi = 0;
@@ -24,7 +22,7 @@ void puts2(char *str)
 	{
 		if (o % 2 == 0)
 	{
-		_puthar(str[0]);
+		_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
